cc6f48dfefd9625fdf5941ea19dd1616de7751a9
Ziyang Li <liby99@seas.upenn.edu>
2020-07-31 23:00:08 +0000

    Add name() getter to generic value

--- src/values/value.rs
+++ src/values/value.rs
@@ -1,11 +1,18 @@
 use llvm_sys::prelude::LLVMValueRef;
 use std::marker::PhantomData;
 
+use crate::utils::*;
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct GenericValue<'ctx>(LLVMValueRef, PhantomData<&'ctx ()>);
 
 impl<'ctx> GenericValue<'ctx> {
   pub(crate) fn new(ptr: LLVMValueRef) -> Self {
     Self(ptr, PhantomData)
+  }
+
+  pub fn name(&self) -> Option<String> {
+    let s = string_of_value(self.0);
+    if s.len() == 0 { None } else { Some(s) }
   }
 }
