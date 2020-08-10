6f3e200b0072a011ca78892b3f1e7fbf9a7c92d4
Ziyang Li <liby99@seas.upenn.edu>
2020-08-10 04:06:00 +0000

    Add inline asm function type getter

--- src/values/inline_asm.rs
+++ src/values/inline_asm.rs
@@ -1,10 +1,18 @@
+use llvm_sys::core::*;
 use llvm_sys::prelude::LLVMValueRef;
 use std::marker::PhantomData;
 
 use crate::*;
+use crate::types::*;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct InlineAsm<'ctx>(LLVMValueRef, PhantomData<&'ctx ()>);
+
+impl<'ctx> InlineAsm<'ctx> {
+  pub fn function_type(&self) -> FunctionType<'ctx> {
+    FunctionType::from_llvm(unsafe { LLVMGetElementType(LLVMTypeOf(self.0)) })
+  }
+}
 
 impl<'ctx> FromLLVMValue for InlineAsm<'ctx> {
   fn from_llvm(ptr: LLVMValueRef) -> Self {
